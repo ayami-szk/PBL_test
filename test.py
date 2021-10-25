466c6ee0e3a805fe52505f805954da6aa01bbc8d
ayami-szk <ayami.suzuki.6i@stu.hosei.ac.jp>
2021-10-25 18:40:56 +0900

    port

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='/static')
@@ -7,6 +8,23 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
     return render_template('index.html')
+
+import sqlite3
+from flask import g
+
+DATABASE = '/path/to/gatabase.db'
+
+def get_db():
+    db = getattr(g, '_databese', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_databese', None)
+    if db is not None:
+        db.close()
 
 # @app.route('/', methods = ['POST'])
 # def from():
@@ -28,4 +46,4 @@ def index():
 #    return "hogehoge!!"
 
 if __name__=='__main__':
-    app.run(debug=True, port=11111)
+    app.run(host = '0.0.0.0', port=os.environ['port'])
